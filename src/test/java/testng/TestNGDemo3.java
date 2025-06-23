fbad68285e35e8eeb0d782e00b132fea1705a6da
avi350751 <avishek350751@gmail.com>
2025-06-22 21:20:28 -0500

    added gitignore file

--- src/test/java/testng/TestNGDemo3.java
+++ src/test/java/testng/TestNGDemo3.java
@@ -5,28 +5,38 @@ import org.testng.annotations.Test;
 
 public class TestNGDemo3 {
 	
-  @Test
+  @Test(priority=-1)
   public void newtest1() {
 	  
-  System.out.println("Login to Application");
-  Assert.assertEquals("Obhi".toLowerCase(), "obhi", "Message Verification failed: ");
+  System.out.println("test1");
+  Assert.assertEquals("Obhi".toLowerCase(), "Obhi", "Message Verification failed: ");
   
   }
   
   @Test
   public void newtest2() {
 	  
-  System.out.println("Login to Application1");
+  System.out.println("test2");
   Assert.assertFalse(false);
   
   }
   
-  @Test
+  @Test(dependsOnMethods="newtest1")
   public void newtest3() {
 	  
-  System.out.println("Login to Application2");
+  System.out.println("test3");
   Assert.assertTrue(false);
   
+  }
+  
+  @Test(priority=-2)
+  public void test4() {
+	  System.out.println("test4");
+  }
+  
+  @Test(priority=1)
+  public void test5() {
+	  System.out.println("test5");
   }
   
 }
